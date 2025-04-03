2852f768306ab258b31903a38d2ed29de7e287f5
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-04-03 06:50:03 +0300

    i18n fix

--- app/routes/$lang/index.tsx
+++ app/routes/$lang/index.tsx
@@ -2,18 +2,22 @@ import { SwitchLanguage } from "@/components/switch-language";
 import { Link } from "@/i18n/link";
 import { createFileRoute } from "@tanstack/react-router";
 import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
 
 export const Route = createFileRoute("/$lang/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { t } = useTranslation();
+
   const handleDummyToast = () => {
     toast("Here is your toast.");
   };
 
   return (
     <main className="mx-auto flex max-w-7xl flex-col gap-4 p-10">
+      <h1>{t("index.title")}</h1>
       <SwitchLanguage />
       <button
         onClick={handleDummyToast}
@@ -26,6 +30,12 @@ function RouteComponent() {
         to="/blog"
       >
         Blog Page
+      </Link>
+      <Link
+        className="w-fit rounded border border-gray-200 bg-gray-100 px-4 py-2 font-medium transition-colors duration-300 hover:opacity-75"
+        to="/not-found"
+      >
+        Not Found Page
       </Link>
     </main>
   );
--- app/routes/$lang/not-found.tsx
+++ app/routes/$lang/not-found.tsx
@@ -1,7 +1,6 @@
-import { createFileRoute, useLayoutEffect, useLocation, useNavigate } from "@tanstack/react-router"; // prettier-ignore
+import { createFileRoute } from "@tanstack/react-router";
 import { seoTranslations } from "@/i18n/languages";
 import { useTranslation } from "react-i18next";
-import { useLanguage } from "@/i18n/use-language";
 import { Link } from "@/i18n/link";
 
 export const Route = createFileRoute("/$lang/not-found")({
@@ -28,32 +27,22 @@ export const Route = createFileRoute("/$lang/not-found")({
 
 export function DefaultNotFound() {
   const { t } = useTranslation();
-  // const { language } = useLanguage();
-  // const navigate = useNavigate();
-  // const location = useLocation();
-
-  // useLayoutEffect(() => {
-  //   const expectedPath = `/${language}/not-found`;
-  //   const isExpectedPath = location.pathname === expectedPath;
-
-  //   if (!isExpectedPath) {
-  //     navigate({ to: "/not-found" });
-  //   }
-  // }, []);
 
   return (
     <div className="flex min-h-[60vh] flex-col items-center justify-center px-4 text-center">
       <h1 className="mb-4 text-6xl font-bold text-gray-800">404</h1>
       <h2 className="mb-6 text-2xl font-semibold text-gray-700">
-        {t("notFoundTitle")}
+        {t("not-found.title")}
       </h2>
-      <p className="mb-8 max-w-md text-gray-600">{t("notFoundDescription")}</p>
+      <p className="mb-8 max-w-md text-gray-600">
+        {t("not-found.description")}
+      </p>
       <Link
         to={`/`}
         preload={false}
         className="rounded bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600"
       >
-        {t("backToHome")}
+        {t("not-found.link")}
       </Link>
     </div>
   );
